b9fbaac5660d6303c8a5a783108a241ef3098928
Dennis Staal <dennis@staal-mail.dk>
2021-02-25 09:45:40 +0100

    Updated config

--- config.h
+++ config.h
@@ -8,15 +8,42 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel       = 0;
 static const char *fonts[]          = { "monospace:size=13" };
 static const char dmenufont[]       = "monospace:size=13";
+
+//static const char col_gray1[]       = "#222222";
+//static const char col_gray2[]       = "#444444";
+//static const char col_gray3[]       = "#bbbbbb";
+//static const char col_gray4[]       = "#eeeeee";
+//static const char col_cyan[]        = "#005577";
+//static const char *colors[][3]      = {
+//	/*               fg         bg         border   */
+//	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+//	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+//};
+
+/*
+DRACULA COLOR SCHEME
+Background	#282a36	
+Current Line	#44475a	
+Foreground	#f8f8f2	
+Comment		#6272a4	
+Cyan		#8be9fd	
+Green		#50fa7b	
+Orange		#ffb86c	
+Pink		#ff79c6	
+Purple		#bd93f9	
+Red		#ff5555	
+Yellow		#f1fa8c
+*/
+
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray4[]       = "#f8f8f2";
+static const char col_cyan[]        = "#44475a";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_gray1,  col_cyan  },
 };
 
 /* tagging */
